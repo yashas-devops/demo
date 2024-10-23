97079ac8f35f400af819f97aaa0ec531e5d2436b
yashas <yashas@email.com>
2024-10-23 10:02:21 +0530

    commited successfully

--- 1.py
+++ 1.py
@@ -4,3 +4,9 @@ sum=a-b
 print(sub)
 print{"substration program}
 print{"= is an assignment operator"}
+
+#printing add number from 1 to 10
+for n in range(0,10):
+    if (n%2!==0):
+        print(n)
+        
